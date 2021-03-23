f1de7e4eae4a5bd98fedef56739a716df1adab4f
Gaston Deseine <gaston.deseine@ig2i.centralelille.fr>
2021-03-23 12:02:26 +0100

    Init first component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EvenementComponent} from './journal/evenement/evenement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/evenement', pathMatch: 'full' },
+  { path: 'evenement', component: EvenementComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
